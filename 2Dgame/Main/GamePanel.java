21daae809db35c3942cf11530b455de567623d59
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-12-18 18:05:45 -0500

    optional delta/accum game loop

--- 2Dgame/Main/GamePanel.java
+++ 2Dgame/Main/GamePanel.java
@@ -25,6 +25,7 @@ public class GamePanel extends JPanel implements Runnable {
     int playerX = 100;
     int playerY = 100;
     int playerSpeed = 4;
+
     public GamePanel() {
         this.setPreferredSize(new Dimension(screenWidth, screenHeight));
         this.setBackground(Color.black);
@@ -40,52 +41,88 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread = new Thread(this);
         gameThread.start();
     }
+
     @Override
     public void run() {
 
-        double drawInterval = 1000000000/FPS; //9 zeros => 1 billion / 1 nanosecond ... drawInterval becomes 16,666,666.6666 or screen draw occurs every .01666667 seconds
+        double drawInterval = 1000000000 / FPS;
         double nextDrawTime = System.nanoTime() + drawInterval;
+
         while (gameThread != null) {
-            //   System.out.println("Game is running");
 
             // UPDATE: update information such as character positions
             update();
             //   DRAW: draw the screen with the update information
             repaint();
 
-
             try {
-                double remainingTime = nextDrawTime - System.nanoTime();
-                remainingTime = remainingTime/1000000; //divide remaining time by 1mill to convert it for below sleep method which is in milliseconds
+                double remainingTime = (nextDrawTime - System.nanoTime()) / 1000000;
 
-                if (remainingTime < 0) { remainingTime = 0};
+                if(remainingTime < 0) {
+                    remainingTime = 0;
+                }
                 Thread.sleep((long) remainingTime);
 
                 nextDrawTime += drawInterval;
+
             } catch (InterruptedException e) {
-                e.printStackTrace();
+                throw new RuntimeException(e);
             }
 
         }
+    }
 
-    public void update() {
-        if (keyH.upPressed == true) {
-            playerY -= playerSpeed;
-        } else if (keyH.downPressed == true) {
-            playerY += playerSpeed;
-        } else if (keyH.rightPressed == true) {
-            playerX += playerSpeed;
-        } else if (keyH.leftPressed == true) {
-            playerX -= playerSpeed;
+
+    //ANOTHER GAME LOOP APPROACH - DELTA/ACCUMULATOR LOOP
+
+    /*
+
+    public static void run() {
+
+        double drawInterval = 100000000/FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+        double nextDrawTime = System.nanoTime() + drawInterval;
+
+        while (gameThread != null) {
+
+            currentTime = system.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if (delta > 1) {
+                update();
+                repaint();
+                delta--;
+            }
+        }
+
+    }
+
+     */
+        public void update () {
+            if (keyH.upPressed == true) {
+                playerY -= playerSpeed;
+            } else if (keyH.downPressed == true) {
+                playerY += playerSpeed;
+            } else if (keyH.rightPressed == true) {
+                playerX += playerSpeed;
+            } else if (keyH.leftPressed == true) {
+                playerX -= playerSpeed;
+            }
+            ;
+        }
+
+        public void paintComponent (Graphics g){
+            super.paintComponent(g);
+
+            Graphics2D g2 = (Graphics2D) g;
+            g2.setColor(Color.white);
+            g2.fillRect(playerX, playerY, charDimensions, charDimensions);
+            g2.dispose();
         }
     }
 
-    public void paintComponent(Graphics g) {
-        super.paintComponent(g);
-
-        Graphics2D g2 = (Graphics2D)g;
-        g2.setColor(Color.white);
-        g2.fillRect(playerX,playerY,charDimensions,charDimensions);
-        g2.dispose();
-    }
-}
