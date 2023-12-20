4ccdda0350ecd3f81841e501cc5b2a2401e513c3
AgileMyk <90538513+AgileMyk@users.noreply.github.com>
2023-12-19 22:24:09 -0500

    dbld images, counter in update - movement ani

--- 2Dgame/entity/Player.java
+++ 2Dgame/entity/Player.java
@@ -3,7 +3,10 @@ package entity;
 import main.GamePanel;
 import main.KeyHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 
 public class Player extends Entity {
 
@@ -15,6 +18,7 @@ public class Player extends Entity {
         this.keyH = keyH;
 
         setDefaultValues();
+        getPlayerImage();
     }
 
     public void setDefaultValues() {
@@ -22,21 +26,94 @@ public class Player extends Entity {
         y = 100;
         speed = 4;
 
+        direction = "right";
+
     }
 
     public void update() {
         if (keyH.upPressed == true) {
+            direction = "up";
             y -= speed;
         } else if (keyH.downPressed == true) {
+            direction = "down";
             y += speed;
         } else if (keyH.rightPressed == true) {
+            direction = "right";
             x += speed;
         } else if (keyH.leftPressed == true) {
+            direction = "left";
             x -= speed;
         }
+
+        spriteCounter++;
+        if (spriteCounter > 15) {
+            if (spriteNum == 1) {
+                spriteNum = 2;
+            }
+            else if (spriteNum == 2) {
+                spriteNum = 1;
+            }
+            spriteCounter = 0;
+        }
+
     }
+
     public void draw(Graphics2D g2) {
-        g2.setColor(Color.white);
-        g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+     //   g2.setColor(Color.white);
+     //   g2.fillRect(x, y, gp.tileSize, gp.tileSize);
+
+        BufferedImage image = null;
+        switch (direction) {
+            case "up":
+                if (spriteNum == 1) {
+                    image = up1;
+                }
+                if (spriteNum == 2) {
+                    image = up2;
+                }
+                break;
+            case "down":
+                if (spriteNum == 1) {
+                    image = down1;
+                }
+                if (spriteNum == 2) {
+                    image = down2;
+                }
+                break;
+            case "right":
+                if (spriteNum == 1) {
+                    image = right1;
+                }
+                if (spriteNum == 2) {
+                    image = right2;
+                }
+                break;
+            case "left":
+                if (spriteNum == 1) {
+                    image = left1;
+                }
+                if (spriteNum == 2) {
+                    image = left2;
+                }
+                break;
+        }
+        g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
+    }
+
+    public void getPlayerImage() {
+
+        try {
+            up1 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharUp.png"));
+            up2 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharUp2.png"));
+            down1 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharDown.png"));
+            down2 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharDown2.png"));
+            right1 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharRight.png"));
+            right2 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharRight2.png"));
+            left1 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharLeft.png"));
+            left2 = ImageIO.read(getClass().getResourceAsStream("/player/playerCharLeft2.png"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
