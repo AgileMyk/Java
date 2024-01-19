86399e660bd5bd236c75b1a9a0a5ba0cd1c266b3
g(m hall) <agilemike@protonmail.com>
2024-01-18 20:36:37 -0500

    began task: ProcessingStrings

--- hyperskill/ProcessingStrings.java
+++ hyperskill/ProcessingStrings.java
@@ -1,3 +1,37 @@
+/*
+Cutting out the middle of a string
+Write a program that reads a string, and then outputs the string without its
+middle character when the length is odd, and without the middle 2 characters
+when the length is even.
+
+Hint: Remember substring() method. It can help you a lot.
+
+Sample Input 1:
+Hello
+
+Sample Output 1:
+Helo
+
+Sample Input 2:
+abcd
+
+Sample Output 2:
+ad
+ */
 public class ProcessingStrings {
 
+    public static void main(String[] args) {
+
+        String what = "Test me out";
+
+        System.out.println(stringProcessor(what));
+    }
+
+    public static String stringProcessor(String s) {
+        if (s.length() % 2 == 0) {
+            return s.substring(s.length() /2, s.length() / 2 -2);
+        } else {
+            return s.substring(s.length() / 2, s.length() /2 - 1);
+        }
+    }
 }
