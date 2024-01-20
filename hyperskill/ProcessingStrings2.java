9ee057ff80d3532419c1660dff1d81fda4e0e4b3
g(m hall) <agilemike@protonmail.com>
2024-01-20 09:53:45 -0500

    finish task: ProcessingStrings(substring v)

--- hyperskill/ProcessingStrings2.java
+++ hyperskill/ProcessingStrings2.java
@@ -5,6 +5,10 @@ public class ProcessingStrings2 {
         Scanner in = new Scanner(System.in);
         String s = in.nextLine();
         int n = s.length() / 2 + s.length() % 2 - 1;
+        System.out.println(s.length() % 2 - 1);
+        System.out.println("n is: "+n);
+        System.out.print("(0,n): " + s.substring(0,n));
+        System.out.println(s.substring(s.length() - n));
         System.out.println(s.substring(0, n) + s.substring(s.length() - n));
 
     }
