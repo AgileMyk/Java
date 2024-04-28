dc20f233056784952803c681822332cad0e5dc8e
g(m hall) <agilemike@protonmail.com>
2024-04-28 18:52:53 -0400

    ToDoApp; import menu button northPanel

--- toDoApp/src/MainToDoJFrame.java
+++ toDoApp/src/MainToDoJFrame.java
@@ -1,9 +1,12 @@
 import javax.print.attribute.standard.Finishings;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 public class MainToDoJFrame {
     private JFrame frame;
     private JPanel northPanel;
+    private JLabel northLabel;
+    private JButton menuButton;
     private JPanel centerPanel;
     private JPanel southPanel;
     public MainToDoJFrame() {
@@ -25,17 +28,25 @@ public class MainToDoJFrame {
         //Panels
         //NORTH
         northPanel = new JPanel();
-        northPanel.setSize(450,175);
-        northPanel.setPreferredSize(new Dimension(450, 125));
+        northPanel.setSize(frame.getWidth(),125);
+        northPanel.setPreferredSize(new Dimension(frame.getWidth(), 125));
         northPanel.setLayout(new FlowLayout(FlowLayout.LEFT));
         northPanel.setBackground(Color.lightGray);
+        northPanel.setFont(new Font("Arial", Font.BOLD, 18));
         //northPanel.add(new JButton("north"));
         frame.add(northPanel, BorderLayout.NORTH);
+        northPanel.setBorder(new EmptyBorder(10,20,10,20));
+        northLabel = new JLabel("Task name here(insert object title)");
+        menuButton = createMenuButton();
+        menuButton.setPreferredSize(new Dimension(38, 38));
+        menuButton.setContentAreaFilled(false);
+        northPanel.add(menuButton);
+
 
         //CENTER
         centerPanel = new JPanel();
-        centerPanel.setSize(450,400);
-        centerPanel.setPreferredSize(new Dimension(450,400));
+        centerPanel.setSize(frame.getWidth(),400);
+        centerPanel.setPreferredSize(new Dimension(frame.getWidth(),400));
         centerPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         centerPanel.setBackground(Color.darkGray);
 
@@ -43,10 +54,18 @@ public class MainToDoJFrame {
 
         //SOUTH
         southPanel = new JPanel();
-        southPanel.setSize(450,200);
+        southPanel.setSize(frame.getWidth(),200);
         southPanel.setPreferredSize(new Dimension(250, 250));
         southPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
         northPanel.setBackground(Color.lightGray);
         frame.add(southPanel, BorderLayout.SOUTH);
+    }
+
+    private JButton createMenuButton() {
+        JButton menuButton = new JButton();
+        ImageIcon menuIcon = new ImageIcon("toDoApp/src//img/menu.png");
+        menuButton.setIcon(menuIcon);
+
+        return menuButton;
     }
 }
