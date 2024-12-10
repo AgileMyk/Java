b239e9369cfb2b2922669dc9510f509999b5bc53
agileMyk <agilemike@protonmail.com>
2024-12-10 14:41:04 -0500

    began Exercise_12_1; research cmdln java args input to arraylist

---
+++ CMSC315/Exercise_11_8/Exercise_12_1.java
@@ -0,0 +1,17 @@
+/*
+(NumberFormatException) Listing 7.9, Calculator.java, is a simple commandline calculator. Note that the program terminates if any
+operand is nonnumeric.
+Write a program with an exception handler that deals with nonnumeric operands;
+then write another program without using an exception handler to achieve the
+same objective. Your program should display a message that informs the user of
+the wrong operand type before exiting (see Figure 12.12).
+ */
+
+
+//7.9
+
+package Exercise_11_8;
+
+
+public class Exercise_12_1 {
+}
