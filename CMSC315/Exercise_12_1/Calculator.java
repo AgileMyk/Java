22b3f4c9db025e61c3793940b9e5bee3038f765c
agileMyk <agilemike@protonmail.com>
2024-12-10 20:27:02 -0500

    began Exercise_12_1; input stop function; test for appropriate value inputs

--- CMSC315/Exercise_12_1/Calculator.java
+++ CMSC315/Exercise_12_1/Calculator.java
@@ -5,38 +5,63 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Calculator {
+
+            final static String stopOne = "s".toLowerCase();
+            final static String stopTwo = "stop".toLowerCase();
+            final static String[] OPERATORS = {"+","-","/","*"};
   /** Main method */
           public static void main(String[] args) {
-         // Check number of strings passed
-              System.out.println(System.getProperty("CMSC315>Exercise_12_1.Calculator.class"));
-              String[] operators = {"+","-","/","*"};
-              ArrayList<String> userInput = new ArrayList<>(Arrays.asList(operators));
-              Scanner in = new Scanner(Arrays.toString(args));
+              // Check number of strings passed
+              ArrayList<String> userInput = new ArrayList<>();
 
+              System.out.println("""
+                      We are going to perform some calculations.
+                      Please enter a series of values, alternating between integer operators
+                      and operands. The result will show.
+                      -----
+                      Type 's' or 'stop' to stop the entering of values.""");
+              //Scanner in = new Scanner(Arrays.toString(userInput.toArray()));
+              Scanner in = new Scanner(System.in);
+              //checking if inputs were valid values/ integer or operator
               while (in.hasNext()) {
-                    userInput.add(in.next());
-              }
+                  if (in.next().equals(stopOne) || in.next().equals(stopTwo)) {
+                      System.exit(0);
+                  }
+                  if (!in.next().equals(OPERATORS[0]) ||
+                      !in.next().equals(OPERATORS[1]) ||
+                      !in.next().equals(OPERATORS[2]) ||
+                      !in.next().equals(OPERATORS[3]) ||
+                      !in.next().matches("[0-9]")) {
+                      System.out.println("In invalid value was entered; either non-integer or non operator ");
+                      return;
+                    }
+                  }
+                  userInput.add(in.next());
+
+
+              System.out.println(userInput);
               //check if last input is an operator and correct
-              if (userInput.getLast().equals(operators[0]) ||
-                      userInput.get(userInput.size() - 2).equals(operators[1]) ||
-                      userInput.get(userInput.size() - 2).equals(operators[2]) ||
-                      userInput.get(userInput.size() - 2).equals(operators[3])) {
+              if (userInput.getLast().equals(OPERATORS[0]) ||
+                      userInput.getLast().equals(OPERATORS[1]) ||
+                      userInput.getLast().equals(OPERATORS[2]) ||
+                      userInput.getLast().equals(OPERATORS[3])) {
                   userInput.removeLast();
                   System.out.println("""
-                                      The last input entered was not legal; an operator instead of an operand
-                                      removing last input and calculating as if last input was not an operand""");
+                          The last input entered was not legal; an operator instead of an operand -
+                          removing last input and calculating as if last input was not an operand""");
               }
               //check if first input is an operator and correct
-              if (userInput.getFirst().equals(operators[0]) ||
-                      userInput.get(userInput.size() - 2).equals(operators[1]) ||
-                      userInput.get(userInput.size() - 2).equals(operators[2]) ||
-                      userInput.get(userInput.size() - 2).equals(operators[3])) {
+              if (userInput.getFirst().equals(OPERATORS[0]) ||
+                      userInput.getFirst().equals(OPERATORS[1]) ||
+                      userInput.getFirst().equals(OPERATORS[2]) ||
+                      userInput.getFirst().equals(OPERATORS[3])) {
                   userInput.removeFirst();
                   System.out.println("""
-                                      The last input entered was not legal; an operator instead of an operand
-                                      removing last input and calculating as if last input was not an operand""");
+                          The last input entered was not legal; an operator instead of an operand -
+                          removing last input and calculating as if last input was not an operand""");
               }
 
+/*
               if (args.length != 3) {
              System.out.println(
                      "Usage: java Calculator operand1 operator operand2");
@@ -64,5 +89,23 @@ public class Calculator {
          // Display result
          System.out.println(args[0] + ' ' + args[1] + ' ' + args[2]
                  + " = " + result);
-         }
+
+ */
+          }
+    public static int perCalcResult(String first, String second, String third) {
+            if (second.equals(OPERATORS[0])) {
+                return Integer.parseInt(first) + Integer.parseInt(third);
+            } else if (second.equals(OPERATORS[1])) {
+                return Integer.parseInt(first) - Integer.parseInt(third);
+            } else if (second.equals(OPERATORS[2])) {
+                return Integer.parseInt(first) / -Integer.parseInt(third);
+            } else if (second.equals(OPERATORS[3])) {
+                return Integer.parseInt(first) * -Integer.parseInt(third);
+            } else {
+                System.out.println("There was an error with operator placement; exiting");
+                return 0;
+            }
+    }
  }
+
+
