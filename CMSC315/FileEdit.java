7b2b3ff06c8ee097d5106750b9113e1e3e2e6330
agileMyk <agilemike@protonmail.com>
2024-12-10 19:13:25 -0500

    began Exercise_12_1; research cmdln java args input to arraylist

--- CMSC315/FileEdit.java
+++ CMSC315/FileEdit.java
@@ -1,17 +1,42 @@
 import java.io.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Scanner;
 
 public class FileEdit {
 
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, MalformedURLException {
 
+        /*
         File sourceFile = new File("C:\\Users\\agile\\IdeaProjects\\Java\\CMSC315\\domains.txt");
         System.out.println(sourceFile.exists());
         System.out.println(sourceFile.isFile());
         File sourceFileEdit = new File("domainsEdit.txt");
+        System.out.println(sourceFileEdit.exists());
+        System.out.println(sourceFileEdit.isFile());
+         */
 
+
+
+        try {
+            URL url = new URL("https://biblehub.com");
+            Scanner inWeb = new Scanner(url.openStream());
+            int count = 1;
+            while (inWeb.hasNextLine()) {
+                String line = inWeb.nextLine();
+                System.out.println(inWeb.nextLine());
+                System.out.println("line " + count + " has a length of " + line.length());
+                count++;
+            }
+        } catch (MalformedURLException e1) {
+            System.out.println("Invalid URL");
+        } catch (IOException e2) {
+            System.out.println("IO Exception");
+        }
+
+       /*
         try (
                     FileWriter fw = new FileWriter(sourceFileEdit, true);
                     Scanner sc1 = new Scanner(sourceFile)
@@ -23,5 +48,6 @@ public class FileEdit {
                     fw.append(line2).append(line1).append("\r\n");
                 }
             }
+        */
     }
 }
