8e5035dddbee5b34bb824d0a2988d4bf580cb121
agileMyk <agilemike@protonmail.com>
2024-12-12 19:34:22 -0500

    began Exercise_12_1; change condition scanner reading next/nextInt to nextLine

--- CMSC315/Exercise_12_1/Calculator.java
+++ CMSC315/Exercise_12_1/Calculator.java
@@ -11,8 +11,8 @@ public class Calculator {
             final static String[] OPERATORS = {"+","-","/","*"};
   /** Main method */
           public static void main(String[] args) {
-              // Check number of strings passed
-              ArrayList<String> userInput = new ArrayList<>();
+
+              Scanner in = new Scanner(System.in);
 
               System.out.println("""
                       We are going to perform some calculations.
@@ -20,21 +20,135 @@ public class Calculator {
                       and operands. The result will show.
                       -----
                       Type 's' or 'stop' to stop the entering of values.""");
+              String input = in.nextLine();
+              System.out.println(input);
+              char[] allInputs = input.toCharArray();
+              ArrayList<String> OperatorInputs = new ArrayList<>();
+              ArrayList<String> OperandInputs = new ArrayList<>();
+              int total = 0;
+              int count = 0;
+              for (char ele: allInputs) {
+                  if (String.valueOf(ele).matches(OPERATORS[0]) ||
+                      String.valueOf(ele).matches(OPERATORS[1]) ||
+                      String.valueOf(ele).matches(OPERATORS[2]) ||
+                      String.valueOf(ele).matches(OPERATORS[3])) {
+                      OperatorInputs.add(String.valueOf(ele));
+                  } else if (String.valueOf(ele).matches("\\d")) {
+                      OperandInputs.add(String.valueOf(ele));
+                  }
+                  /*
+
+                  ) && count == 0) {
+                      total = Integer.parseInt(ele);
+                      count++;
+
+                  } else if (ele.matches(OPERATORS[0]) ||
+                             ele.matches(OPERATORS[1]) ||
+                             ele.matches(OPERATORS[2]) ||
+                             ele.matches(OPERATORS[3])) {
+                      OperatorInputs.add(ele);
+                  } else if (ele.matches("\\d") && count != 0) {
+                      OperatorInputs.add(ele);
+                  } else {
+                      System.out.println("The input " + ele + " is invalid. Skipping to next input");
+                  }
+                  for (String s : OperatorInputs) {
+                      System.out.print(s + " ");
+                  }
+                  for (String s : OperandInputs) {
+                      System.out.print(s + " ");
+                  }
+                  */
+              }
+              /*
+
               //Scanner in = new Scanner(Arrays.toString(userInput.toArray()));
               Scanner in = new Scanner(System.in);
               //checking if inputs were valid values/ integer or operator
-              while (in.hasNext()) {
-                  if (in.next().equals(stopOne) || in.next().equals(stopTwo)) {
-                      System.exit(0);
+              int total = 0;
+              ArrayList<String> Operands = new ArrayList<>();
+              ArrayList<String> Operators = new ArrayList<>();
+              int i = 0;
+              int j = 0;
+              if (in.hasNext() && !in.next().equals(stopOne) || !in.next().equals(stopTwo)) {
+                  if (in.next().equals(OPERATORS[0]) ||
+                      in.next().equals(OPERATORS[1]) ||
+                      in.next().equals(OPERATORS[2]) ||
+                      in.next().equals(OPERATORS[3])) {
+                      Operands.add(in.next());
+                      System.out.println("entered value: " + Operands.get(i));
+                      System.out.println(Operands.get(i) + " added to Operands ArrayList");
+                      i++;
+                  } else if (in.nextInt() && !in.nextInt().equals(stopOne) || !in.next().equals(stopTwo)) {
+                      Operands.add(in.next());
+                      System.out.println("entered value: " + Operands.get(j));
+                      System.out.println(Operands.get(j) + " added to Operands ArrayList");
+                      j++;
                   }
+              }
+
+              for (String ele : userInput) {
+                  if (ele.equals(OPERATORS[0]) ||
+                          ele.equals(OPERATORS[1]) ||
+                          ele.equals(OPERATORS[2]) ||
+                          ele.equals(OPERATORS[3])) {
+                      System.out.println(ele + " added to operands");
+                      Operands.add(ele);
+                  }
+                  if (ele.matches("\\d")) {
+                      System.out.println(ele + " added to operators");
+                      Operators.add(ele);
+                  }
+              }
+              //String tempTotal = Operands.getFirst();
+              Operands.removeFirst();
+              while (!Operators.isEmpty() && !Operands.isEmpty()) {
+                    total = perCalcResult(String.valueOf(total), Operators.getFirst(), Operands.getFirst());
+                    Operators.removeFirst();
+                    Operands.removeFirst();
+                    System.out.println(total);
+              }
+               */
+          }
+
+    public static int perCalcResult(String first, String second, String third) {
+        if (second.equals(OPERATORS[0])) {
+            return Integer.parseInt(first) + Integer.parseInt(third);
+        } else if (second.equals(OPERATORS[1])) {
+            return Integer.parseInt(first) - Integer.parseInt(third);
+        } else if (second.equals(OPERATORS[2])) {
+            return Integer.parseInt(first) / -Integer.parseInt(third);
+        } else if (second.equals(OPERATORS[3])) {
+            return Integer.parseInt(first) * -Integer.parseInt(third);
+        } else {
+            System.out.println("There was an error with operator placement; exiting");
+            return 0;
+        }
+    }
+              /*
+                 System.out.print("count: "+count);
+                  if (in.next().equals(stopOne) || in.next().equals(stopTwo)) {
+                      return;
+                  }
+                  //checks if invalid values (non operator/operand) are entered
                   if (!in.next().equals(OPERATORS[0]) ||
                       !in.next().equals(OPERATORS[1]) ||
                       !in.next().equals(OPERATORS[2]) ||
                       !in.next().equals(OPERATORS[3]) ||
                       !in.next().matches("[0-9]")) {
-                      System.out.println("In invalid value was entered; either non-integer or non operator ");
+                      System.out.println("An invalid value was entered; either non-integer or non operator ");
                       return;
                     }
+                  //error if an operator is entered at the wrong time
+                  if (count % 2 == 0 && !in.next().equals(OPERATORS[0]) ||
+                          !in.next().equals(OPERATORS[1]) ||
+                          !in.next().equals(OPERATORS[2]) ||
+                          !in.next().equals(OPERATORS[3])) {
+                      System.out.print("""
+                                        An operand was entered at the wrong time.
+                                        Value entry should reflect the following patter:
+                                        operand/operator/operand/operator, etc., not ending with an operator.""");
+                  }
                   }
                   userInput.add(in.next());
 
@@ -60,6 +174,7 @@ public class Calculator {
                           The last input entered was not legal; an operator instead of an operand -
                           removing last input and calculating as if last input was not an operand""");
               }
+               */
 
 /*
               if (args.length != 3) {
@@ -91,21 +206,8 @@ public class Calculator {
                  + " = " + result);
 
  */
-          }
-    public static int perCalcResult(String first, String second, String third) {
-            if (second.equals(OPERATORS[0])) {
-                return Integer.parseInt(first) + Integer.parseInt(third);
-            } else if (second.equals(OPERATORS[1])) {
-                return Integer.parseInt(first) - Integer.parseInt(third);
-            } else if (second.equals(OPERATORS[2])) {
-                return Integer.parseInt(first) / -Integer.parseInt(third);
-            } else if (second.equals(OPERATORS[3])) {
-                return Integer.parseInt(first) * -Integer.parseInt(third);
-            } else {
-                System.out.println("There was an error with operator placement; exiting");
-                return 0;
-            }
-    }
+
+
  }
 
 
