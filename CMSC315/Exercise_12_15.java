ab4e0296e48b07e47d0ccbb4586e994fa7f16154
agilemyk <agilemike@protonmail.com>
2024-12-22 11:32:59 -0500

    -

---
+++ CMSC315/Exercise_12_15.java
@@ -0,0 +1,9 @@
+/*
+(Write/read data) Write a program to create a file named Exercise12_15.txt if
+it does not exist. Write 100 integers created randomly into the file using text
+I/O. Integers are separated by spaces in the file. Read the data back from the
+file and display the data in increasing order.
+ */
+
+public class Exercise_12_15 {
+}
