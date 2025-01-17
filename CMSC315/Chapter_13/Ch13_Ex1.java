bb7a3d9f4ef95e2c941e3ebff46be5b1008d4b18
AgileMyk <AgileMike@protonmail.com>
2025-01-16 21:07:26 -0500

    Exercise 13_1;

---
+++ CMSC315/Chapter_13/Ch13_Ex1.java
@@ -0,0 +1,30 @@
+package Chapter_13;
+/*
+(Shuffle ArrayList) Write the following method that shuffles an ArrayList
+of numbers:
+
+ */
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Ch13_Ex1 {
+    public static void main(String[] args) {
+        ArrayList<Integer> test1 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            test1.add(i);
+        }
+        for (int i : test1) {
+            System.out.println(i);
+        }
+        ArrayListShuffle(test1);
+        System.out.println(test1);
+    }
+
+    public static void ArrayListShuffle(ArrayList<Integer> a) {
+        Collections.shuffle(a);
+    }
+}
+
+
+
