c98898ed804705c4d56d88c48491347edac10965
AgileMyk <AgileMike@protonmail.com>
2025-02-14 14:15:48 -0500

    CMSC345 wk6 testing changes 1; roomTypes[], reservation()(37-45), correctRoomTypeStatement()(25-30)

---
+++ CMSC345/jarWk6/Reservation.java
@@ -0,0 +1,124 @@
+package jarWk6;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Objects;
+import java.util.Scanner;
+import java.util.UUID;
+
+public class Reservation {
+    private String datePattern = "MMM dd, yyyy";
+    private UUID reservationID = UUID.randomUUID();
+    private Date reservationDate = new Date();
+    private int guestID;
+    private String roomType;
+    private String reservationStartDate;
+    private String reservationEndDate;
+    private long reservationNumberOfDays;
+    private String[] roomTypes = {"RoomWBath", "RoomWView", "NormalRoom"};
+
+    public String OverBook() {
+        return "not done";
+    }
+
+    //NEW
+    public void correctRoomTypeStatement() {
+        System.out.println("please enter a valid room type.");
+        for (String roomType : roomTypes) {
+            System.out.println(roomType);
+        }
+    }
+    //NEW
+    public void correctRoomType() {
+        correctRoomTypeStatement();
+    }
+
+    // CHANGES MADE
+    public Reservation(int var1, String var2, String var3, String var4) {
+        this.guestID = var1;
+        Scanner room = new Scanner(System.in);
+        if (!Objects.equals(var2, roomTypes[0]) ||
+            !Objects.equals(var2, roomTypes[1]) ||
+            !Objects.equals(var2, roomTypes[2])) {
+            correctRoomType();
+            setRoom(room.next().trim());
+        }
+        this.roomType = var2;
+        this.reservationStartDate = var3;
+        this.reservationEndDate = var4;
+    }
+
+    public UUID getReservationID() {
+        return this.reservationID;
+    }
+
+    public Date getReservationDate() {
+        return this.reservationDate;
+    }
+
+    public int getGuestID() {
+        return this.guestID;
+    }
+
+    public String getRoomType() {
+        return this.roomType;
+    }
+
+    public String getReservationStartDate() {
+        return this.reservationStartDate;
+    }
+
+    public String getReservationEndDate() {
+        return this.reservationEndDate;
+    }
+
+    public void setGuestID(int var1) {
+        this.guestID = var1;
+    }
+
+    // CHANGES MADE
+    public void setRoom(String var1) {
+        while (!Objects.equals(var1, roomTypes[0]) ||
+                !Objects.equals(var1, roomTypes[1]) ||
+                !Objects.equals(var1, roomTypes[2])) {
+            correctRoomTypeStatement();
+            setRoom(var1);
+        }
+        this.roomType = var1;
+    }
+
+    public void setReservationStartDate(String var1) {
+        this.reservationEndDate = var1;
+    }
+
+    public void setReservationEndDate(String var1) {
+        this.reservationEndDate = var1;
+    }
+
+    public boolean validateDate(String datePattern) {
+        return false;
+    }
+
+    public long calculateReversationNumberOfDays() throws Exception {
+        long var1 = (new SimpleDateFormat(this.datePattern)).parse(this.reservationStartDate).getTime();
+        long var3 = (new SimpleDateFormat(this.datePattern)).parse(this.reservationEndDate).getTime();
+        this.reservationNumberOfDays = (var3 - var1) / 8640000L;
+        return (var3 - var1) / 86400000L;
+    }
+
+    public double calculateReservationBillAmount() throws Exception {
+        if (this.roomType.equals("RoomWBath")) {
+            return (double)(200L * this.reservationNumberOfDays);
+        } else if (this.roomType.equals("RoomWView")) {
+            return (double)(175L * this.calculateReversationNumberOfDays());
+        } else {
+            return this.roomType.equals("NormalRoom") ? (double)(120L * this.calculateReversationNumberOfDays()) : 0.0D;
+        }
+    }
+
+    public void updateReservation() {
+    }
+
+    public void dropReservation() {
+    }
+}
---
+++ CMSC345/jarWk6/test1.java
@@ -0,0 +1,71 @@
+package jarWk6;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+public class test1 {
+
+    static String equalsInBG() {
+        return " \u001b[43mequals\u001b[0m ";
+    }
+
+    static String doesNotEqualInBG() {
+        return " \u001b[43mdoes not equal\u001b[0m ";
+    }
+
+    static String expextedlLabel() {
+        return "Expected:";
+    }
+
+    public static void main(String[] args) throws ParseException {
+
+        //date test
+        System.out.println("var1");
+        long var1 = (new SimpleDateFormat("MMM dd, yyyy")).parse("feb 7, 2025").getTime();
+        System.out.println("var3");
+        long var3 = (new SimpleDateFormat("MMM dd, yyyy")).parse("feb 14, 2025").getTime();
+        long resOther = var3 - var1;
+        long reservationNumberOfDays = (var3 - var1) / 8640000L;
+        System.out.println("res num:" + reservationNumberOfDays);
+        //ERROR
+        System.out.println("res num2:" + (var3 - var1) / 86400000L);
+        System.out.println("res num4:" + (var3 - var1) % 86400000L);
+        //CORRECT
+        System.out.println("res num3:" + (resOther) / 86400000L);
+        System.out.println((var3 - var1) / 86400000L);
+
+        String ANSI_RED = "\u001b[31m";
+        String ANSI_BLACK = "\u001b[30m";
+        String ANSI_WHITE = "\u001b[37m";
+        String ANSI_BG_RED = "\u001b[41m";
+        String ANSI_BG_GREEN = "\u001b[42m";
+        String ANSI_BG_CYAN = "\u001b[46m";
+        String ANSI_BG_YELLOW = "\u001b[43m";
+        String ANSI_RESET = "\u001b[0m";
+        System.out.println("red:");
+        System.out.println(ANSI_RED);
+        System.out.println("black:");
+        System.out.println(ANSI_BLACK);
+        System.out.println("white:");
+        System.out.println(ANSI_WHITE);
+        System.out.println("bg-red:");
+        System.out.println(ANSI_BG_RED);
+        System.out.println("bg-green:");
+        System.out.println(ANSI_BG_GREEN);
+        System.out.println("bg-cyan:");
+        System.out.println(ANSI_BG_CYAN);
+        System.out.println("bg-yellow:");
+        System.out.println(ANSI_BG_YELLOW);
+        System.out.println("reset:");
+        System.out.println(ANSI_RESET);
+
+        double var0 = 5.5;
+        double var2 = 5.5;
+
+        System.out.println("Actual" + var0 + equalsInBG() + expextedlLabel() + var2);
+
+
+        System.out.println(equalsInBG());
+        System.out.println(doesNotEqualInBG());
+    }
+}
