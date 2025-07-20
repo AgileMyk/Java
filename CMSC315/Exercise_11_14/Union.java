f5f0c0f44c2a77915d25f8676da68899ac321977
AgileMyk <AgileMike@protonmail.com>
2025-07-20 14:30:33 -0400

    getType() adjust, printing empty string if first val not valid choice

--- CMSC315/Exercise_11_14/Union.java
+++ CMSC315/Exercise_11_14/Union.java
@@ -7,17 +7,17 @@ public static ArrayList<Integer> union(
 ArrayList<Integer> list1, ArrayList<Integer> list2)
  */
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.Scanner;
 
 public class Union {
 
-    private static final String[] typesList = {"short", "int", "integer", "long", "float", "double",
-            "boolean", "char", "string", "date", "array", "object"
-    };
+    private static final String[] typesList = {"short", "int", "integer", "long", "float",
+                                  "double", "boolean", "char", "string", "date", "object"};
 
     static Scanner sc = new Scanner(System.in);
-    private ArrayList list1 = new ArrayList();
     private ArrayList list2 = new ArrayList();
 
     public static void main(String[] args) {
@@ -26,20 +26,26 @@ public class Union {
                 "if you choose a primitive type, the resulting data type will be the associated wrapper object");
 
         System.out.println("Please choose a data type:");
-        String type1 = sc.next().trim();
+        String type1 = sc.next().trim().toLowerCase();
 
         String finalInput = getType(type1);
         System.out.println("You've chosen " + finalInput + " as your data type in the ArrayList");
+
+        //ArrayList list1 = createArrayList(type1);
+        //System.out.println(list1);
+
+
     }
 
     //cycles through available data types, testing against valid choices, returns valid chosen type
     public static String getType(String input) {
+        String returnVal = "";
         boolean b = false;
-        int index = 0;
         for (int i = 0; i < typesList.length; i++) {
-            if (input.trim().toLowerCase().equals(typesList[i])) {
-                index = i;
+            if (input.equals(typesList[i])) {
                 b = true;
+                returnVal = typesList[i];
+                break;
             }
         }
         if (!b) {
@@ -49,12 +55,47 @@ public class Union {
             }
             System.out.println();
             String additionalAttempt = sc.next().trim();
-            getType(additionalAttempt);
+            returnVal = getType(additionalAttempt);
         }
-        //System.out.println(index);
-        //System.out.println(typesList[index]);
-        return typesList[index];
+        //System.out.println("returnVal: " + returnVal);
+        return returnVal;
 
+    }
+
+    public static ArrayList<? extends Object> createArrayList(String var) {
+        if (var.equals("short")) {
+            return new ArrayList<Short>();
+        } else if (var.equals("int")) {
+            return new ArrayList<Integer>();
+        } else if (var.equals("integer")) {
+            return new ArrayList<Integer>();
+        } else if (var.equals("long")) {
+            return new ArrayList<Long>();
+        } else if (var.equals("float")) {
+            return new ArrayList<Float>();
+        } else if (var.equals("double")) {
+            return new ArrayList<Double>();
+        } else if (var.equals("boolean")) {
+            return new ArrayList<Boolean>();
+        } else if (var.equals("char")) {
+            return new ArrayList<Character>();
+        } else if (var.equals("string")) {
+            return new ArrayList<String>();
+        } else if (var.equals("date")) {
+            return new ArrayList<Date>();
+        } else if (var.equals("object")) {
+            return new ArrayList<>();
+        } else {
+            System.out.println("Please choose a valid data type: ");
+            for (String type : typesList) {
+                System.out.print(" " + type);
+            }
+            System.out.println();
+            String input = sc.next().trim();
+            createArrayList(input);
+        }
+        System.out.println("exiting; creating a new ArrayList of Objects    ");
+        return new ArrayList<>();
     }
 }
 /*
