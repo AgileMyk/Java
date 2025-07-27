90b86b2ecc0fc5939888398c2ed24a9a5f3a0ae9
AgileMyk <AgileMike@protonmail.com>
2025-07-27 16:45:23 -0400

    dataAddQuery() check for invalid decimal entries across all appropriate methods; verbal feedback for createArrayList()

--- CMSC315/Exercise_11_14/Union.java
+++ CMSC315/Exercise_11_14/Union.java
@@ -1,5 +1,10 @@
 package Exercise_11_14;
 
+//NEXT:
+// ADD POPULATE CREATED ARRAY METHODS
+// CHECK IF ARRAYS COMPATIBLE METHOD
+// COMBINE ARRAYS AND RETURN NEW ARRAY
+
 /*
 11.14 (Combine two lists) Write a method that returns the
 union of two array lists of integers using the following header:
@@ -36,18 +41,9 @@ public class Union {
 
         System.out.println("You have created an arrayList that can contain the following data types: " + finalInput);
 
-        dataAddQuery();
-
-
-        //System.out.println("Please choose a second data type:");
-        //String type2 = sc.next().trim().toLowerCase();
-
-        //ask for arraylist with type
-        //String finalInput2 = getType(type2);
-        //dataAddQuery(type2);
-
-
-
+        String lengthOfArray = dataAddQuery();
+        System.out.println("your array: " + arrList1);
+        System.out.println("length:" + lengthOfArray);
     }
 
     //cycles through available data types, testing against valid choices, returns valid chosen type
@@ -77,69 +73,85 @@ public class Union {
 
     public static ArrayList<? extends Object> createArrayList(String var) {
         if (var.equals("short")) {
+            System.out.println("you have created an ArrayList containing shorts(wrapped as Short) ");
             return new ArrayList<Short>();
         } else if (var.equals("int")) {
+            System.out.println("you have created an ArrayList containing int(wrapped as Integer) ");
             return new ArrayList<Integer>();
         } else if (var.equals("integer")) {
+            System.out.println("you have created an ArrayList containing Integers) ");
             return new ArrayList<Integer>();
         } else if (var.equals("long")) {
+            System.out.println("you have created an ArrayList containing long(wrapped as Long) ");
             return new ArrayList<Long>();
         } else if (var.equals("float")) {
+            System.out.println("you have created an ArrayList containing float(wrapped as Float) ");
             return new ArrayList<Float>();
         } else if (var.equals("double")) {
+            System.out.println("you have created an ArrayList containing double(wrapped as Double) ");
             return new ArrayList<Double>();
         } else if (var.equals("boolean")) {
+            System.out.println("you have created an ArrayList containing booleans");
             return new ArrayList<Boolean>();
         } else if (var.equals("char")) {
+            System.out.println("you have created an ArrayList containing chars");
             return new ArrayList<Character>();
         } else if (var.equals("string")) {
+            System.out.println("you have created an ArrayList containing strings");
             return new ArrayList<String>();
         } else if (var.equals("date")) {
+            System.out.println("you have created an ArrayList containing Dates");
             return new ArrayList<Date>();
         } else if (var.equals("object")) {
+            System.out.println("you have created an ArrayList containing objects");
             return new ArrayList<>();
         }
         System.out.println("An arrayList containing the " + var + " data type");
         return new ArrayList<>();
     }
 
-    public static long dataAddQuery() {
-        System.out.println("Would you like to add items?: ");
+    public static String dataAddQuery() {
+        String answer = "";
+        System.out.println("Would you like to add items? ");
         String choice = sc.next().trim().toLowerCase();
-        long i = 0;
         if (choice.equals("yes".toLowerCase()) || choice.equals("y".toLowerCase())) {
             System.out.println("How many items?");
-            String answer = sc.next().trim().toLowerCase();
+            answer = sc.next().trim().toLowerCase();
+
+
             //check if input is a valid numerical value
-            String stringNumber = "";
             boolean numStatus = false;
             for (int j = 0; j < answer.length(); j++) {
-                if (Character.isDigit(answer.charAt(j))) {
+                if (Character.isDigit(answer.charAt(j)) && !answer.contains(".")) {
                     numStatus = true;
                 } else {
                     numStatus = false;
                 }
             }
             //convert string representation of valid number value to long data type
-            if (numStatus) {
-                i = Long.parseLong(answer);
-            } else {
-                System.out.println("invalid value entry: " + answer);
-            }
-        } else if (choice.equals("no".toLowerCase()) || choice.equals("n".toLowerCase())) {
+
+                if (numStatus) {
+                    return answer;
+                } else {
+                    System.out.println("""
+                            Please ensure your answer is a whole number
+                            Let's begin again
+                            """);
+                    answer = dataAddQuery();
+                }
+            } else if (choice.equals("no".toLowerCase()) || choice.equals("n".toLowerCase())) {
             System.out.println("Good bye");
         } else {
             System.out.println("Please enter yes/y or no/n");
             dataAddQuery();
         }
-        System.out.println("number of items: " + i);
-        return i;
+        return answer;
     }
-
 
     public static void addPrompt(String dataType, ArrayList arrList ) {
         System.out.println("Would you like to add items to " + arrList +"?");
     }
+
 }
 
 /*
