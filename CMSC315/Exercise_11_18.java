7a2ae41d1e8becc16c8a9af46dccad2e9ebb9d67
AgileMyk <AgileMike@protonmail.com>
2025-08-28 18:11:21 -0400

    exe 11_18; complete

---
+++ CMSC315/Exercise_11_18.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercise_11_18 {
+/*
+ (ArrayList of Character) Write a method that returns
+an array list of Character from a string using the following
+header:
+public static ArrayList<Character>
+toCharacterArray(String s)
+For example, toCharacterArray("abc") returns an array list
+that contains characters 'a' , 'b' , and 'c' .
+
+ */
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("please type a word: ");
+        String word = sc.next();
+
+        ArrayList<Character> alist = toCharacterArray(word);
+        for (Character c : alist) {
+            System.out.println(c);
+        }
+    }
+
+    public static ArrayList<Character> toCharacterArray(String s) {
+        ArrayList<Character> l = new ArrayList<>();
+        for (char c : s.toCharArray()) {
+            l.add(c);
+        }
+        return l;
+    }
+
+}
